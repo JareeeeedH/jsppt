a34bd4da2a5e17bddb99140ea5a5f0a9b19ae46d
Jared <jared_huang@mail.chungyo.net>
2021-10-05 16:08:58 +0800

    first commit

---
+++ demoCode/modulesDemo/app.js
@@ -0,0 +1,17 @@
+// import {greeting, userData as myData} from './data.js';
+// import sayGoodbyd from './data.js';
+
+// console.log(greeting);
+// console.log(myData);
+
+
+import sayGoodbyd, { userData as myData, greeting } from './data.js';
+
+greeting('jared');
+console.log(myData)
+sayGoodbyd()
+
+
+
+// import * as dataList from './data.js';
+// console.log(dataList)
---
+++ demoCode/modulesDemo/data.js
@@ -0,0 +1,11 @@
+export function greeting(name){
+	console.log(`hi!${name}`);
+}
+
+export const userData = {name:'jared', age:29, hometown: 'KH'};
+
+
+
+export default function sayGoodBye(){
+	console.log('bye! jared ...');
+}
